b0f4fa0edbf7e6efc4962d658d1ea03784c1b72d
Lucas Amancio <lhamancio@gmail.com>
2023-08-08 08:45:46 -0300

    rotas do app

---
+++ src/pages/Principal/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import {
+  PrincipalView,
+  DescricaoPasso
+} from "./styles";
+
+
+export default function Main ({ navigation }){
+
+ return(
+  <PrincipalView>
+    <DescricaoPasso>pagina principal</DescricaoPasso>
+  </PrincipalView>
+  )
+}
---
+++ src/pages/Principal/styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components/native";
+
+export const PrincipalView = styled.View`
+  flex-direction: column;
+  align-items: center;
+  background-color : #fff;
+  flex: 1
+`;
+export const DescricaoPasso = styled.Text`
+  font-size: 20px;
+  padding: 10px 12px;
+  background-color : #fff;
+`;
